62c100d4a27f0fa7044bfa0309902aea4d6c7cea
hrmoh <mohammadi.hr@gmail.com>
2017-02-23 10:50:35 +0000

    gsync2vid: missing files

---
+++ gsync2vid/GTransitionEffect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gsync2vid
+{
+    /// <summary>
+    /// افکت انتقال بین قابها
+    /// </summary>
+    public enum GTransitionEffect
+    {
+        None = 0,
+        ToRight = 1,
+        ToUp = 2
+    }
+}
