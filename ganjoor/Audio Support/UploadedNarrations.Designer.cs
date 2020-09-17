c82504f693aa613e584362d5e5b5c48ffb5245d9
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-09-17 22:05:56 +0430

    اسکلت فرم خوانشهای بارگذاری شده

---
+++ ganjoor/Audio Support/UploadedNarrations.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ganjoor.Audio_Support
+{
+    partial class UploadedNarrations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl = new System.Windows.Forms.Panel();
+            this.grd = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl.Location = new System.Drawing.Point(0, 0);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(867, 33);
+            this.pnl.TabIndex = 0;
+            // 
+            // grd
+            // 
+            this.grd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grd.Location = new System.Drawing.Point(0, 33);
+            this.grd.Name = "grd";
+            this.grd.RowHeadersWidth = 62;
+            this.grd.RowTemplate.Height = 28;
+            this.grd.Size = new System.Drawing.Size(867, 447);
+            this.grd.TabIndex = 1;
+            // 
+            // UploadedNarrations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 480);
+            this.Controls.Add(this.grd);
+            this.Controls.Add(this.pnl);
+            this.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.MinimizeBox = false;
+            this.Name = "UploadedNarrations";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "خوانشهای بارگذاری شده";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.grd)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.DataGridView grd;
+    }
+}
---
+++ ganjoor/Audio Support/UploadedNarrations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ganjoor.Audio_Support
+{
+    public partial class UploadedNarrations : Form
+    {
+        public UploadedNarrations()
+        {
+            InitializeComponent();
+        }
+    }
+}
