14176981e45f00d325b7de85e41e516c495f98d6
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-07-09 19:55:49 +0430

    نمایش پیش فرض دکمهٔ کپی متن

--- ganjoor/Properties/Settings.Designer.cs
+++ ganjoor/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ganjoor.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -181,7 +181,7 @@ namespace ganjoor.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool CopyButtonVisible {
             get {
                 return ((bool)(this["CopyButtonVisible"]));
