5377e6e6b9afc550ae30cb1836ff231d77a1f669
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-07-13 22:31:36 +0430

    v2.94

--- ganjoor/Properties/AssemblyInfo.cs
+++ ganjoor/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.93.*")]
-[assembly: AssemblyFileVersion("2.9.3.0")]
+[assembly: AssemblyVersion("2.94.*")]
+[assembly: AssemblyFileVersion("2.9.4.0")]
 [assembly: NeutralResourcesLanguageAttribute("fa-IR")]
