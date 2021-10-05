f162c289925dd0e6299ad32cec5d9f6fbd21da8f
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-10-05 09:41:45 +0330

    fixning ShowInTaskbar for two tool windows

--- ganjoor/Editor/MrgTwoClmns.Designer.cs
+++ ganjoor/Editor/MrgTwoClmns.Designer.cs
@@ -49,9 +49,11 @@
             // 
             this.groupBox1.Controls.Add(this.clmn1);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.groupBox1.Location = new System.Drawing.Point(0, 33);
+            this.groupBox1.Location = new System.Drawing.Point(0, 50);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(245, 479);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(368, 718);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "ستون اول:";
@@ -59,10 +61,11 @@
             // clmn1
             // 
             this.clmn1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.clmn1.Location = new System.Drawing.Point(3, 16);
+            this.clmn1.Location = new System.Drawing.Point(4, 24);
+            this.clmn1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.clmn1.Multiline = true;
             this.clmn1.Name = "clmn1";
-            this.clmn1.Size = new System.Drawing.Size(239, 460);
+            this.clmn1.Size = new System.Drawing.Size(360, 690);
             this.clmn1.TabIndex = 0;
             this.clmn1.TextChanged += new System.EventHandler(this.clmn1_TextChanged);
             // 
@@ -70,9 +73,11 @@
             // 
             this.groupBox2.Controls.Add(this.clmn2);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.groupBox2.Location = new System.Drawing.Point(245, 33);
+            this.groupBox2.Location = new System.Drawing.Point(368, 50);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(245, 479);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(368, 718);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "ستون دوم:";
@@ -80,10 +85,11 @@
             // clmn2
             // 
             this.clmn2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.clmn2.Location = new System.Drawing.Point(3, 16);
+            this.clmn2.Location = new System.Drawing.Point(4, 24);
+            this.clmn2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.clmn2.Multiline = true;
             this.clmn2.Name = "clmn2";
-            this.clmn2.Size = new System.Drawing.Size(239, 460);
+            this.clmn2.Size = new System.Drawing.Size(360, 690);
             this.clmn2.TabIndex = 0;
             this.clmn2.TextChanged += new System.EventHandler(this.clmn1_TextChanged);
             // 
@@ -91,9 +97,11 @@
             // 
             this.groupBox3.Controls.Add(this.result);
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.groupBox3.Location = new System.Drawing.Point(490, 33);
+            this.groupBox3.Location = new System.Drawing.Point(736, 50);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(245, 479);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(368, 718);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "نتیجه:";
@@ -101,10 +109,11 @@
             // result
             // 
             this.result.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.result.Location = new System.Drawing.Point(3, 16);
+            this.result.Location = new System.Drawing.Point(4, 24);
+            this.result.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.result.Multiline = true;
             this.result.Name = "result";
-            this.result.Size = new System.Drawing.Size(239, 460);
+            this.result.Size = new System.Drawing.Size(360, 690);
             this.result.TabIndex = 0;
             this.result.TextChanged += new System.EventHandler(this.result_TextChanged);
             // 
@@ -116,8 +125,9 @@
             this.panel1.Controls.Add(this.btnMerge);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(734, 33);
+            this.panel1.Size = new System.Drawing.Size(1101, 50);
             this.panel1.TabIndex = 1;
             // 
             // chkAutomaticFocus
@@ -126,9 +136,10 @@
             this.chkAutomaticFocus.AutoSize = true;
             this.chkAutomaticFocus.Checked = true;
             this.chkAutomaticFocus.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkAutomaticFocus.Location = new System.Drawing.Point(330, 8);
+            this.chkAutomaticFocus.Location = new System.Drawing.Point(493, 12);
+            this.chkAutomaticFocus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkAutomaticFocus.Name = "chkAutomaticFocus";
-            this.chkAutomaticFocus.Size = new System.Drawing.Size(119, 17);
+            this.chkAutomaticFocus.Size = new System.Drawing.Size(181, 25);
             this.chkAutomaticFocus.TabIndex = 3;
             this.chkAutomaticFocus.Text = "تغییر خودکار فوکوس ";
             this.chkAutomaticFocus.UseVisualStyleBackColor = true;
@@ -139,9 +150,10 @@
             this.chkIgnoreBlankLines.AutoSize = true;
             this.chkIgnoreBlankLines.Checked = true;
             this.chkIgnoreBlankLines.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkIgnoreBlankLines.Location = new System.Drawing.Point(453, 9);
+            this.chkIgnoreBlankLines.Location = new System.Drawing.Point(673, 14);
+            this.chkIgnoreBlankLines.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkIgnoreBlankLines.Name = "chkIgnoreBlankLines";
-            this.chkIgnoreBlankLines.Size = new System.Drawing.Size(164, 17);
+            this.chkIgnoreBlankLines.Size = new System.Drawing.Size(253, 25);
             this.chkIgnoreBlankLines.TabIndex = 2;
             this.chkIgnoreBlankLines.Text = "خطوط خالی نادیده گرفته شوند";
             this.chkIgnoreBlankLines.UseVisualStyleBackColor = true;
@@ -151,9 +163,10 @@
             this.btnInsert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnInsert.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnInsert.Enabled = false;
-            this.btnInsert.Location = new System.Drawing.Point(185, 3);
+            this.btnInsert.Location = new System.Drawing.Point(278, 4);
+            this.btnInsert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnInsert.Name = "btnInsert";
-            this.btnInsert.Size = new System.Drawing.Size(133, 23);
+            this.btnInsert.Size = new System.Drawing.Size(200, 34);
             this.btnInsert.TabIndex = 1;
             this.btnInsert.Text = "درج نتیجه در ویرایشگر";
             this.btnInsert.UseVisualStyleBackColor = true;
@@ -162,9 +175,10 @@
             // 
             this.btnMerge.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnMerge.Enabled = false;
-            this.btnMerge.Location = new System.Drawing.Point(623, 6);
+            this.btnMerge.Location = new System.Drawing.Point(934, 9);
+            this.btnMerge.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMerge.Name = "btnMerge";
-            this.btnMerge.Size = new System.Drawing.Size(99, 23);
+            this.btnMerge.Size = new System.Drawing.Size(148, 34);
             this.btnMerge.TabIndex = 0;
             this.btnMerge.Text = "یک در میان چیدن";
             this.btnMerge.UseVisualStyleBackColor = true;
@@ -172,18 +186,20 @@
             // 
             // MrgTwoClmns
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(734, 512);
+            this.ClientSize = new System.Drawing.Size(1101, 768);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MrgTwoClmns";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.RightToLeftLayout = true;
             this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "یک در میان چیدن خطوط دو ستون متنی";
             this.groupBox1.ResumeLayout(false);
--- ganjoor/Editor/ReplaceInDb.Designer.cs
+++ ganjoor/Editor/ReplaceInDb.Designer.cs
@@ -34,11 +34,11 @@
             this.btnHeye = new System.Windows.Forms.Button();
             this.btnNumbers = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtFind = new System.Windows.Forms.TextBox();
-            this.txtRep = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.btnReplace = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRep = new System.Windows.Forms.TextBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,10 +48,10 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(600, 13);
+            this.label1.Size = new System.Drawing.Size(924, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "تذکر مهم: لطفاً قبل از استفاده از این ابزار از پایگاه داده‌های برنامه پشتیبان تهی" +
-                "ه کنید. محدودهٔ عملکرد شامل تمام پایگاه داده‌هاست.";
+    "ه کنید. محدودهٔ عملکرد شامل تمام پایگاه داده‌هاست.";
             // 
             // btnYaKaf
             // 
@@ -101,42 +101,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "جایگزینی دلخواه:";
             // 
-            // txtFind
-            // 
-            this.txtFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtFind.Location = new System.Drawing.Point(6, 23);
-            this.txtFind.Name = "txtFind";
-            this.txtFind.Size = new System.Drawing.Size(479, 21);
-            this.txtFind.TabIndex = 1;
-            // 
-            // txtRep
-            // 
-            this.txtRep.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtRep.Location = new System.Drawing.Point(6, 53);
-            this.txtRep.Name = "txtRep";
-            this.txtRep.Size = new System.Drawing.Size(479, 21);
-            this.txtRep.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(491, 26);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "متن مورد جستجو:";
-            // 
-            // label3
-            // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(491, 53);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "متن جایگزین:";
-            // 
             // btnReplace
             // 
             this.btnReplace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -148,9 +112,45 @@
             this.btnReplace.UseVisualStyleBackColor = true;
             this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
             // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(491, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "متن جایگزین:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(491, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "متن مورد جستجو:";
+            // 
+            // txtRep
+            // 
+            this.txtRep.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRep.Location = new System.Drawing.Point(6, 53);
+            this.txtRep.Name = "txtRep";
+            this.txtRep.Size = new System.Drawing.Size(479, 27);
+            this.txtRep.TabIndex = 3;
+            // 
+            // txtFind
+            // 
+            this.txtFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFind.Location = new System.Drawing.Point(6, 23);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(479, 27);
+            this.txtFind.TabIndex = 1;
+            // 
             // ReplaceInDb
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(627, 253);
             this.Controls.Add(this.groupBox1);
@@ -165,6 +165,8 @@
             this.Name = "ReplaceInDb";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "جایگزینی در پایگاه داده‌ها";
             this.groupBox1.ResumeLayout(false);
