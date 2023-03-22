c0dc3e977948043da3a355cac89d085b75b7770b
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-03-22 19:12:45 +0330

    version 2.98

--- ganjoor/Properties/AssemblyInfo.cs
+++ ganjoor/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("حمیدرضا محمدی")]
 [assembly: AssemblyProduct("گنجور")]
-[assembly: AssemblyCopyright("GPL, 1388-1401, ganjoor.net, Hamid Reza Mohammadi")]
+[assembly: AssemblyCopyright("GPL, 1388-1402, ganjoor.net, Hamid Reza Mohammadi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.97.*")]
-[assembly: AssemblyFileVersion("2.9.7.0")]
+[assembly: AssemblyVersion("2.98.*")]
+[assembly: AssemblyFileVersion("2.9.8.0")]
 [assembly: NeutralResourcesLanguageAttribute("fa-IR")]
